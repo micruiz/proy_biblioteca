a426354463b235523280707ce4b17b91bb20e273
micruiz <136381068+micruiz@users.noreply.github.com>
2023-07-24 11:01:14 -0500

    Add files via upload

---
+++ bdd_biblioteca.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.5-10.4.28-MariaDB : Database - bdd_biblioteca
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bdd_biblioteca` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `bdd_biblioteca`;
+
+/*Table structure for table `tab_libros` */
+
+CREATE TABLE `tab_libros` (
+  `id_libro` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_libro` varchar(255) NOT NULL,
+  `descripcion_libro` text NOT NULL,
+  `autor_libro` varchar(60) NOT NULL,
+  `edi_libro` varchar(60) NOT NULL,
+  `fecha_libro` date NOT NULL,
+  `foto_libro` blob NOT NULL,
+  PRIMARY KEY (`id_libro`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
